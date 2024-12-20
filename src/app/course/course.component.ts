79dcbb4e50775e56cc9c3de2c6570e84fef3c88a
Vince-Colson-TM <vincecolson@thomasmore.be>
2024-12-20 21:39:46 +0100

    fix text color tag filter light mode

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,9 +1,9 @@
-import { Component, computed, EventEmitter, inject, Input, input, Output, signal } from '@angular/core';
-import { Course, Tag } from '../course';
-import { NgClass, NgStyle } from '@angular/common';
-import { MatDialog } from '@angular/material/dialog';
-import { CourseDetailsModalComponent } from '../course-details-modal/course-details-modal.component';
-import { MatIconModule } from '@angular/material/icon';
+import {Component, computed, EventEmitter, inject, Input, input, Output, signal} from '@angular/core'
+import {Course, Tag} from '../course'
+import {NgClass, NgStyle} from '@angular/common'
+import {MatDialog} from '@angular/material/dialog'
+import {CourseDetailsModalComponent} from '../course-details-modal/course-details-modal.component'
+import {MatIconModule} from '@angular/material/icon'
 
 @Component({
   selector: 'app-course',
@@ -12,46 +12,55 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './course.component.css'
 })
 export class CourseComponent {
-  readonly course = input.required<Course>();
-  readonly isHighlighted = input.required<boolean>();
-  readonly isTagIncluded = input.required<boolean>();
-  readonly isReused = input.required<boolean>();
-  readonly selectedTag = input.required<Tag | null>();
-  @Output() courseClick: EventEmitter<number> = new EventEmitter<number>();
-  readonly dialog = inject(MatDialog);
+  readonly course = input.required<Course>()
+  readonly isHighlighted = input.required<boolean>()
+  readonly isTagIncluded = input.required<boolean>()
+  readonly isReused = input.required<boolean>()
+  readonly selectedTag = input.required<Tag | null>()
+  @Output() courseClick: EventEmitter<number> = new EventEmitter<number>()
+  readonly dialog = inject(MatDialog)
 
   currentNgClass = computed(() => {
-    if (this.isHighlighted()) { return 'bg-orange-700' };
+      if (this.isHighlighted()) {
+        return 'bg-orange-700'
+      }
 
-    if (!this.course().phase_is_mandatory) {
-      return 'bg-green-800'
-    }
-    return 'bg-gray-700';
-  },
-  );
+
+      if (!this.course().phase_is_mandatory) {
+        return 'bg-green-800'
+      }
+
+      if (this.isTagIncluded() && this.selectedTag()) {
+        return 'text-tertiary'
+      }
+
+      return 'bg-gray-700'
+    },
+  )
 
   currentNgStyle = computed(() => {
-    // if (this.isHighlighted()) { return { 'background-color': 'orange' } };
-    if (this.isTagIncluded() && this.selectedTag()) {
-      return { 'background-color': this.selectedTag()!.hex_color }
-    };
-    return {};
-  },
-  );
+      // if (this.isHighlighted()) { return { 'background-color': 'orange' } };
+      if (this.isTagIncluded() && this.selectedTag()) {
+        return {'background-color': this.selectedTag()!.hex_color}
+      }
+
+      return {}
+    },
+  )
 
   onCourseClick() {
-    this.courseClick.emit(this.course().learning_track_id);
+    this.courseClick.emit(this.course().learning_track_id)
   }
 
   openDetails(event: Event, course: any): void {
-    event.stopPropagation();
+    event.stopPropagation()
     this.dialog.open(CourseDetailsModalComponent, {
       data: course,
       width: '90vw',
       height: '90vh',
       maxWidth: '100vw', // Prevent modal from being constrained by default maxWidth
       maxHeight: '100vh',
-    });
+    })
   }
 
   setSelectedTagHighlight() {
